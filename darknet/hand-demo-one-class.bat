9580a9317b799cb00505f2b6f3fcd7e730b81d0c
abars <abarsceo@gmail.com>
2018-11-30 08:48:43 +0900

    Update training script

--- darknet/hand-demo-one-class.bat
+++ darknet/hand-demo-one-class.bat
@@ -1,1 +1,1 @@
-darknet.exe detector demo data/face-one-class.data cfg/yolov2-tiny-train-one-class.cfg c:/deeplearning/backup_face_one_class/yolov2-tiny-train-one-class_32600.weights -c 0
+darknet.exe detector demo data/hand-one-class.data cfg/yolov2-tiny-train-one-class.cfg c:/deeplearning/backup/yolov2-tiny-train-one-7000.weights -c 0
---
+++ darknet/hand-image-one-class.bat
@@ -0,0 +1,1 @@
+darknet.exe detector test data/hand-one-class.data cfg/yolov2-tiny-train-one-class.cfg c:/deeplearning/backup/yolov2-tiny-train-one-class_28500.weights hand_test.png -i 0 
